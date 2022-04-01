1646ae604fb605201828c9f836d90fc147ea42ca
Jonathan Eyolfson <jon@eyolfson.com>
2022-04-01 06:00:18 -0700

    Whitespace improvements

--- sync/management/commands/__init__.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-
-class Command(BaseCommand):
-    help = 'Synchronizes static files with eyolfson.com'
-
-    def handle(self, *args, **options):
-        self.stdout.write(self.style.SUCCESS('sync'))
