c548c5bddc843811b49071f000f10d54db4a7230
Jonathan Eyolfson <jon@eyolfson.com>
2022-04-01 06:13:47 -0700

    Re-write the robots view

--- www/urls.py
+++ www/urls.py
@@ -3,7 +3,7 @@ from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-from .views import RobotsView
+from .views import robots
 from .sitemaps import StaticViewSitemap
 from courses.sitemaps import courses_sitemap
 
@@ -14,7 +14,7 @@ sitemaps.update(courses_sitemap)
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html"), name='index'),
-    path('robots.txt', RobotsView.as_view(), name='robots'),
+    path('robots.txt', robots, name='robots'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
     path('courses/', include('courses.urls')),
--- www/views.py
+++ www/views.py
@@ -1,14 +1,13 @@
+from django.template.response import TemplateResponse
 from django.urls import reverse
-from django.views.generic import TemplateView
 
-class RobotsView(TemplateView):
-
-    template_name="robots.txt"
-    content_type="text/plain"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['sitemap'] = self.request.build_absolute_uri(
-            reverse('django.contrib.sitemaps.views.sitemap')
-        )
-        return context
+def robots(request):
+    sitemap = request.build_absolute_uri(
+        reverse("django.contrib.sitemaps.views.sitemap")
+    )
+    return TemplateResponse(
+        request,
+        'robots.txt',
+        {"sitemap": sitemap},
+        content_type='text/plain',
+    )
